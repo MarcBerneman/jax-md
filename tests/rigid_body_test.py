bc295ab1687bf3c4f2b63db460d2f0538b663d78
MarcBerneman <marc.berneman@outlook.com>
2025-07-27 16:42:38 +0300

    only 3.12

--- tests/rigid_body_test.py
+++ tests/rigid_body_test.py
@@ -19,7 +19,6 @@ import functools
 from absl.testing import absltest
 from absl.testing import parameterized
 import pytest
-import gc
 
 import numpy as onp
 
@@ -77,8 +76,7 @@ def run_before_and_after_tests(tmpdir):
   # This is a fixture that runs before and after each test.
   # This fixes issue 227 (https://github.com/jax-md/jax-md/issues/277)
   yield # this is where the testing happens
-  # gc.collect()
-  # jax.clear_caches()
+  jax.clear_caches()
 
 # pylint: disable=invalid-name
 class RigidBodyTest(test_util.JAXMDTestCase):
